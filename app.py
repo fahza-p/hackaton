68a3831f91b984cf6430a5dbdcca664b238140a4
Fahza Pergata Syahrul Gufron <fahza@192.168.1.17>
2023-08-15 18:56:11 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import (Flask, request)
 from ai_bot import ConstructIndex, GetPrompt, Query
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfReader
+import os
 # import logging
 # import sys
 
@@ -7,7 +10,11 @@ from ai_bot import ConstructIndex, GetPrompt, Query
 # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = {'pdf'}
+
 app.config["DEBUG"] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 index = ConstructIndex(
     from_path="docs",
@@ -25,13 +32,43 @@ query = Query(dir_path="index")
 def main():
     body = request.get_json()
 
+    promptText = body.get("prompt")
+
     chat_histories = prompt_setup.append_history(body.get("history"))
     sys_prompt, train_chat = prompt_setup.text_qa_tmpl()
 
     query.setup_query(sys_prompt=sys_prompt,
                       train_chat=train_chat, chat_histories=chat_histories)
 
-    return query.generate_chat(body.get("prompt"))
+    if body.get("fileName") and body.get("fileName") != "":
+        promptText = "please review this contract base on knowlage you have, provide response in Indonesian languange:"
+        reader = PdfReader('upload/'+body.get("fileName"))
+        for page in range(len(reader.pages)):
+            text = reader.pages[page].extract_text()
+            promptText += text
+
+    return query.generate_chat(promptText)
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return ('No file part')
+
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return filename
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
